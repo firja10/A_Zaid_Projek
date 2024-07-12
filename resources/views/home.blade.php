5334f6c621270cc452a3228a2156563a523a020e
firja10 <fairuzfirjatullah3@gmail.com>
2024-07-12 20:51:14 +0700

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,7 @@
       <div class="header_section header_bg">
          <div class="container-fluid">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <a class="navbar-brand"href="index.html"><img src="images/logo.png"></a>
+               <a class="navbar-brand"href="index.html"><h2 class="address_text">MABES KOPI</h2></a>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
@@ -43,11 +43,11 @@
                         <a class="nav-link" href="{{url('/')}}">Home</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="about.html">Tentang Kami</a>
+                        <a class="nav-link" href="{{url('/login')}}">Login</a>
                      </li>
-                     <li class="nav-item">
+                     {{-- <li class="nav-item">
                         <a class="nav-link" href="{{url('/ShowQRMenu')}}">Daftar Menu</a>
-                     </li>
+                     </li> --}}
  
                      <li class="nav-item">
                         <a class="nav-link" href="contact.html">Contact</a>
