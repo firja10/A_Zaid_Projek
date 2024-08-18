3d7dd3189b0e0a182a764743f0518ad82eab24bd
firja10 <fairuzfirjatullah3@gmail.com>
2024-08-18 12:47:37 +0700

    Update

--- resources/views/barista/stok_produk.blade.php
+++ resources/views/barista/stok_produk.blade.php
@@ -19,14 +19,7 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            {{-- <h6 class="m-0 font-weight-bold text-primary">   </h6> --}}
-                            {{-- <button class="btn-primary btn"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Produk</button> --}}
 
-                            {{-- <button class="btn-primary btn" data-toggle = "modal" data-target = "#exampleModal"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Produk</button> --}}
-
-                            {{-- <a href="{{url('owner/stok_produk/store')}}" class="btn btn-primary"><i class="fas fa-plus fa-sm text-white-50"></i>
-                                Tambah Produk
-                            </a> --}}
 
                         </div>
                         <div class="card-body">
@@ -98,23 +91,7 @@
                     {{-- <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(256)->generate('https://google.com')) !!} "> --}}
         
 
-
-
-
-            
-           
-
-
-
-
                     </div>
-
-
-
-
-
-
-
 
 
                             <!-- Color System -->
@@ -127,15 +104,6 @@
                             </div>
 
                         </div>
-
-
-
-
-
-
-
-
-
 
 
 
